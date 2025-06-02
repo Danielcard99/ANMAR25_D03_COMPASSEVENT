1383f13d0d170304c25519eda9608f0c62c71d36
Danielcard99 <dc2651065@gmail.com>
2025-06-02 04:15:44 -0300

    feat(user): add validations to create-user.dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export enum UserRole {
+  ORGANIZER = 'organizer',
+  PARTICIPANT = 'participant',
+}
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  @IsPhoneNumber('BR')
+  phone: string;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+}
