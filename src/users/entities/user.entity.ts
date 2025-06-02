77fd10e97a8439b2c9f69dd0d036f13fbc38bef6
Danielcard99 <dc2651065@gmail.com>
2025-06-02 04:17:28 -0300

    feat(user): add User interface in entity.ts

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,14 @@
+import { UserRole } from '../dto/create-user.dto';
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: UserRole;
+  profileImageUrl: string;
+  createdAt: string;
+  updatedAt?: string;
+  isActive: boolean;
+}
