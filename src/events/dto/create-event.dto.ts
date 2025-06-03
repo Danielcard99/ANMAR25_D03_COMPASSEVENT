541245cd0db51bc848f950a5d996da3da646bcea
Danielcard99 <dc2651065@gmail.com>
2025-06-03 14:41:03 -0300

    feat(events): add EventStatus enum to create-event.dto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,6 +1,11 @@
 import { Transform } from 'class-transformer';
 import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
+export enum EventStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
+
 export class CreateEventDto {
   @IsString()
   @IsNotEmpty()
