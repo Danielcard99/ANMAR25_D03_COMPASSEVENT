02c9aba5e804c0448484b3fcac530acd6253f5f3
Danielcard99 <dc2651065@gmail.com>
2025-06-03 12:33:06 -0300

    feat(event): add @IsNotEmpty and @Transform validations to CreateEventDto fields

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,12 +1,19 @@
-import { IsDateString, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateEventDto {
   @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.trim())
   name: string;
 
   @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.trim())
   description: string;
 
   @IsDateString()
+  @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.trim())
   date: string;
 }
