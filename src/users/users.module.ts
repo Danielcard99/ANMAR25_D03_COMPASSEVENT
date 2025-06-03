905af86a4d1e65a509c4c56557cec5fddcf34b87
Danielcard99 <dc2651065@gmail.com>
2025-06-02 21:32:41 -0300

    chore(user): import AuthModule into UserModule for authentication support

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './user.controller';
 import { UsersService } from './users.service';
 import { S3Module } from '../s3/s3.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [S3Module],
+  imports: [S3Module, forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
