288bc0ea1a1bcb4677c69b1bcd0a267058694be9
Danielcard99 <dc2651065@gmail.com>
2025-06-04 13:17:37 -0300

    chore(users): add MailModule to imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { S3Module } from '../s3/s3.module';
 import { AuthModule } from '../auth/auth.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [S3Module, forwardRef(() => AuthModule)],
+  imports: [S3Module, forwardRef(() => AuthModule), MailModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
