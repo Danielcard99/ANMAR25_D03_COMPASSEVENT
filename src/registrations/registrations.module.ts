45d55d3184e441faf0cf3709f968007ba12cf919
Danielcard99 <dc2651065@gmail.com>
2025-06-04 13:12:29 -0300

    chore(registrations): add UsersModule and MailModule imports

--- src/registrations/registrations.module.ts
+++ src/registrations/registrations.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RegistrationsService } from './registrations.service';
 import { RegistrationsController } from './registrations.controller';
 import { EventsModule } from '../events/events.module';
+import { MailModule } from '../mail/mail.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, UsersModule, MailModule],
   controllers: [RegistrationsController],
   providers: [RegistrationsService],
 })
