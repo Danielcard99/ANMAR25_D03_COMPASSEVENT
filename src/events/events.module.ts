a82c6b4994f8134fc55f28023dceeae2b19c09d9
Danielcard99 <dc2651065@gmail.com>
2025-06-04 12:57:30 -0300

    chore(events): import MailModule and UsersModule into EventsModule

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { S3Module } from '../s3/s3.module';
+import { MailModule } from '../mail/mail.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [S3Module],
+  imports: [S3Module, MailModule, UsersModule],
   controllers: [EventsController],
   providers: [EventsService],
   exports: [EventsService],
