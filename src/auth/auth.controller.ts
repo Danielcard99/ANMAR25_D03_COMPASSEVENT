226e9a5e5ad7746a36e471f254389a6ac51f8579
Danielcard99 <dc2651065@gmail.com>
2025-06-04 01:18:08 -0300

    docs(auth): add Swagger decorators to AuthController for API documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Auth Login' })
   async login(@Body() dto: LoginDto) {
     return this.authService.login(dto.email, dto.password);
   }
