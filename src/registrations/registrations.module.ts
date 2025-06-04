87007320ff4bb5f7de6238c6b4dfbe253f414180
Danielcard99 <dc2651065@gmail.com>
2025-06-04 03:42:14 -0300

    chore(registrations): setup registrations module with controller, service, and import EventsModule

---
+++ src/registrations/registrations.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RegistrationsService } from './registrations.service';
+import { RegistrationsController } from './registrations.controller';
+import { EventsModule } from '../events/events.module';
+
+@Module({
+  imports: [EventsModule],
+  controllers: [RegistrationsController],
+  providers: [RegistrationsService],
+})
+export class RegistrationsModule {}
